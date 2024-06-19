cbc9b374120275d5b5f139291717cf273c561d51
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-19 21:45:28 +0100

    two sum

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[test]
 fn case_1() {
     assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
@@ -17,12 +19,16 @@ pub struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        
+        let mut hashmap = HashMap::new();
+
+        for (index, num) in nums.iter().enumerate() {
+            if let Some(other) = hashmap.get(&(target - num)) {
+                return vec![*other as i32, index as i32];
+            };
+
+            hashmap.entry(num).or_insert(index);
+        }
+
         vec![]
     }
-
-    // pub fn print_add(a: i32, b: i32) {
-    //     let sum = a + b;
-    //     println!("{sum}");
-    // }
 }
